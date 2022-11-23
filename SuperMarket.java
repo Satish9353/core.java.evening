d8a73db94d2116c49ddb5ca01450b0a3f5cd0f80
Satish9353 <satishgb.xworkz@gmail.com>
2022-11-23 21:05:30 +0530

    committing java files

---
+++ SuperMarket.java
@@ -0,0 +1,21 @@
+class SuperMarket{
+       static String fruits[]={"mango,apple,grapes,banana"};
+	   
+	   public static void fruits(String fruits[])
+	   {
+		   fruits();
+	   }
+	   
+	   System.out.println("fruits,length");
+	   System.out.println("fruits names::");
+	   {
+	   for(int i=0;i<fruits length;fruits++)
+	   System.out.println("All fruits name")
+	   }
+
+
+
+
+
+
+}
