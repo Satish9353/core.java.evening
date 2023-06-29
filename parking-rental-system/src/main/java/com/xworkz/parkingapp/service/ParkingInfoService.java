afa894355e4f2fe54752734e4c657013510a581a
Satish9353 <118912017+Satish9353@users.noreply.github.com>
2023-06-29 22:52:58 +0530

    Delete parking-rental-system directory

--- parking-rental-system/src/main/java/com/xworkz/parkingapp/service/ParkingInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.parkingapp.service;
-
-import com.xworkz.parkingapp.dto.ParkingInfoDTO;
-
-public interface ParkingInfoService {
-	
-	boolean validateAndSave(ParkingInfoDTO dto);
-
-}
--- parking-rental-system/src/main/java/com/xworkz/parkingapp/service/ParkingService.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.parkingapp.service;
-
-import com.xworkz.parkingapp.dto.ParkingDTO;
-
-public interface ParkingService {
-	
-	ParkingDTO userSignIn(String email,String password);
-
-}
